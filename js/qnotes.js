3e7db59d69f7afc9dedc118bf2cb320d2ff264b9
Shani Shashidharan <67774693+shanishashidharan123@users.noreply.github.com>
2022-05-11 19:54:05 +0530

    Note updates
    Note updated.

---
+++ js/qnotes.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+function YourComponent(){
+    return (
+      <p><strong>Note:</strong> We are Contractors and Web Builders, we have no Shop, just Hire us. We take contracts to build websites at client&#8217;s/customer&#8217;s own places, offices and on Genuine data protection servers. And we are not into free service or Seva kinds, we charge at least static Rs. 16,000 to Dynamic or collaboration Rs. 60,00,000. We are professionals and do not try this at home.</p>
+    );
+};
+ReactDOM.render(<YourComponent />, document.getElementById('Qnotes'));
