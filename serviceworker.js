c60d4bddaa9c6746c45430dfeae88c33ba2c399b
Shani Shashidharan <67774693+shanishashidharan123@users.noreply.github.com>
2022-06-06 10:52:19 +0530

    Service worker updates
    Service worker updated.

--- serviceworker.js
+++ serviceworker.js
@@ -24,28 +24,35 @@ const PRECACHE_URLS = [
   'stylesheet/main.css'
 ];
 
-// The install handler takes care of precaching the resources we always need.
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(PRECACHE)
-      .then(cache => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting())
-  );
+self.addEventListener("install", event => {
+   console.log("Service worker installed");
+});
+self.addEventListener("activate", event => {
+   console.log("Service worker activated");
 });
 
+// The install handler takes care of precaching the resources we always need.
+//self.addEventListener('install', event => {
+//  event.waitUntil(
+//    caches.open(PRECACHE)
+//      .then(cache => cache.addAll(PRECACHE_URLS))
+//      .then(self.skipWaiting())
+//  );
+//});
+
 // The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', event => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete);
-      }));
-    }).then(() => self.clients.claim())
-  );
-});
+//self.addEventListener('activate', event => {
+//  const currentCaches = [PRECACHE, RUNTIME];
+//  event.waitUntil(
+//    caches.keys().then(cacheNames => {
+//      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+//    }).then(cachesToDelete => {
+//      return Promise.all(cachesToDelete.map(cacheToDelete => {
+//        return caches.delete(cacheToDelete);
+//      }));
+//    }).then(() => self.clients.claim())
+//  );
+//});
 
 // The fetch handler serves responses for same-origin resources from a cache.
 // If no response is found, it populates the runtime cache with the response
